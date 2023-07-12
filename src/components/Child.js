54977f8ac86ee1c8fcaaafbbf8dcb2141cd3ffcc
dhanraj-saur <dhananjayverma301@gmail.com>
2023-07-12 23:27:22 +0530

    done

---
+++ src/components/Child.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Child=({changeText}) =>{
+
+  return (
+    <div className='child'>
+        <h3>Child Component</h3>
+        <input type='text' onChange={(e)=>{changeText(e.target.value)}} />
+    </div>
+  )
+}
+export default Child;
---
+++ src/components/Parent.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import Child from './Child';
+
+const Parent = () => {
+    let [value, setValue] = useState("")
+
+    const handleChange = (text) => {
+       
+        setValue(text)
+    }
+    return (
+
+        <div className='parent'>
+
+            <h1>Parent Component</h1>
+            <p> {value}</p>
+            <Child changeText={handleChange} />
+        </div>
+
+    )
+}
+
+export default Parent;
